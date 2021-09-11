d4226876061a6d91d46f326aca048c30cd5b8d50
Jolene <jolenechong7@gmail.com>
2021-09-11 11:50:25 +0800

    my first push for netlify forms

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -58,6 +58,77 @@ function Navbar() {
 
   return (
     <>
+      <div className="menu-icon" onClick={handleClick}>
+        <i className={click ? "fas fa-times" : "fas fa-bars"} />
+        <p style={{ color: "grey" }}>Menu</p>
+      </div>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            <img src={logo} alt="Logo" height="40" />
+          </Link>
+
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <LLink
+                to="/#home"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Home
+              </LLink>
+            </li>
+            <li className="nav-item">
+              <LLink
+                to="#projects"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Projects
+              </LLink>
+            </li>
+            <li className="nav-item">
+              <LLink
+                to="#sideprojects"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Side Projects
+              </LLink>
+            </li>
+            <li>
+              <a
+                href="mailto:jolenechong7@gmail.com"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">CONTACT</Button>}
+        </div>
+        <div className="socials">
+          <a
+            className="fa fa-envelope fa-lg"
+            href="mailto:jolenechong7@gmail.com"
+          ></a>
+
+          <a
+            className="fa fa-linkedin fa-lg"
+            href="https://www.linkedin.com/in/jolenechong7/"
+            target="_blank"
+          />
+          <a
+            className="fa fa-github fa-lg"
+            href="https://github.com/jolenechong"
+            target="_blank"
+          />
+        </div>
+        <div className="theme-toggle" onClick={themeToggle}>
+          <i className={themeClick ? "far fa-moon" : "far fa-sun"} />
+        </div>
+      </nav>
     </>
   );
 }
