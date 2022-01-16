dd353ef29f9697bd1a29832bec14cb4393c58c6b
Jolene <jolenechong7@gmail.com>
2022-01-16 23:04:34 +0800

    editFile: funprojects card, added topics

--- src/components/FunProjectsCard.js
+++ src/components/FunProjectsCard.js
@@ -1,25 +1,43 @@
-import './FunProjectsCard.css'
-import LanguageColor from './LanguageColor'
+import "./FunProjectsCard.css";
+import LanguageColor from "./LanguageColor";
 
 const FunProjectsCard = (props) => {
-    const colors = {
-        JavaScript : "#efdf54",
-        HTML : "#d94b23",
-        Python : "#3f73a6"
-    }
+  const colors = {
+    JavaScript: "#efdf54",
+    HTML: "#d94b23",
+    Python: "#3f73a6",
+    "Jupyter Notebook": "#d15a00",
+  };
 
-    return (
-        <div className='cardRepo'>
-        <h3>{props.name}</h3>
-        <p>{props.description}
-        </p>
-        {props.homepage && <a href={props.homepage} target='_blank'><i className="fas fa-globe"></i></a>}
-        <a href={props.html_url} target='_blank'><i className="fab fa-github"></i></a>
+  return (
+    <div className="cardRepo">
+      <h3>{props.name}</h3>
+      <p>{props.description}</p>
+      <div className="icons-div">
+        {props.homepage && (
+          <a href={props.homepage} target="_blank">
+            <i className="fas fa-globe"></i>
+          </a>
+        )}
+        <a href={props.html_url} target="_blank">
+          <i className="fab fa-github"></i>
+        </a>
+      </div>
 
-        <p className='language'><LanguageColor color={colors[props.language]}></LanguageColor>{props.language}</p>
+      <div className="topics-div">
+        {props.topics.length > 0 ? (
+          props.topics.map((repoDetails) => <span>{repoDetails}</span>)
+        ) : (
+          <p></p>
+        )}
+      </div>
 
+      <p className="language">
+        <LanguageColor color={colors[props.language]}></LanguageColor>
+        {props.language}
+      </p>
     </div>
-    )
-}
+  );
+};
 
-export default FunProjectsCard
+export default FunProjectsCard;
