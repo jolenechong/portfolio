2a580cfe11fd3b01074cd86ef1eec88e75dd6067
Jolene <jolenechong7@gmail.com>
2022-01-17 00:25:09 +0800

    updateFile: added topics, removedd funprojcard files

--- src/components/FunProjects.js
+++ src/components/FunProjects.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import Repo from "./FunProjectsCard";
 import "./FunProjects.css";
+import LanguageColor from "./LanguageColor";
 
 const FunProjects = () => {
   const [repo, setRepo] = useState([]);
+  const colors = {
+    JavaScript: "#efdf54",
+    HTML: "#d94b23",
+    Python: "#3f73a6",
+    "Jupyter Notebook": "#d15a00",
+  };
 
   useEffect(() => {
     fetch("https://api.github.com/users/jolenechong/starred")
@@ -12,22 +18,50 @@ const FunProjects = () => {
       .then((data) => {
         setRepo(data);
       });
-  },[]);
+  }, []);
 
   return (
     <>
       <section id="funProjects">
         <h1>Recent projects from Github</h1>
-        <p>
-          Interested in projects outside of web development?
-        </p>
+        <p>Interested in projects outside of web development?</p>
         <div className="funProjContainer">
           {repo.length > 0 ? (
             repo.map((repoDetails) => (
-              <Repo key={repoDetails.id} {...repoDetails} />
+              <div className="cardRepo">
+                <h3>{repoDetails.name}</h3>
+                <p>{repoDetails.description}</p>
+                <div className="icons-div">
+                  {repoDetails.homepage && (
+                    <a href={repoDetails.homepage} target="_blank">
+                      <i className="fas fa-globe"></i>
+                    </a>
+                  )}
+                  <a href={repoDetails.html_url} target="_blank">
+                    <i className="fab fa-github"></i>
+                  </a>
+                </div>
+
+                <div className="topics-div">
+                  {repoDetails.topics.length > 0 ? (
+                    repoDetails.topics.map((topic) => (
+                      <span>{topic}</span>
+                    ))
+                  ) : (
+                    <p></p>
+                  )}
+                </div>
+
+                <p className="language">
+                  <LanguageColor
+                    color={colors[repoDetails.language]}
+                  ></LanguageColor>
+                  {repoDetails.language}
+                </p>
+              </div>
             ))
           ) : (
-            <Repo repo="LOADING..."/>
+          <p>LOADING...</p>
           )}
         </div>
       </section>
